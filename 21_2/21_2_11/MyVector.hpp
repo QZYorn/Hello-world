28761271ccbcf74bcc1f3e084e84137ef871ad7c
QZYorn <864239480@qq.com>
2021-02-12 16:24:12 +0800

    Vector3

--- 21_2/21_2_11/MyVector.hpp
+++ 21_2/21_2_11/MyVector.hpp
@@ -5,7 +5,7 @@ using namespace std;
 
 typedef int Rank;//秩
 #define DEFAULT_CAPACITY 3//初始容量
-#define INCREMENT 1//每次递增扩容大小
+
 template<class T>
 class MyVector
 {
@@ -15,9 +15,18 @@ private:
 	T *_elem;//维护内容
 
 protected:
+	//内部函数
+	//递增扩容
+	void expandAdd();
+
+	//加倍扩容
+	void expandDouble();
+
+	//缩容
+	void shrink();
+
 
 public:
-
 	//构造函数
 	//默认 构造函数
 	MyVector(int c = DEFAULT_CAPACITY);
@@ -42,26 +51,92 @@ public:
 
 
 	//只读接口
+	//读取 秩 元素
+	T& get(Rank i);
+
+	//重载[]运算符
+	T& operator[](Rank r);
+
 	//返回向量大小
 	Rank size();
 
 	//返回向量容量
 	int capacity();
 
-	//重载[]运算符
-	T operator[](Rank i);
+	
 
 
 
 	//只写接口
-	//递增扩容
-	void expendAdd();
+	//将 秩i 元素 换成 指定元素
+	void put(Rank r, T t);
 
-	//加倍扩容
-	void expendDouble();
+	//在秩r处插入元素t,返回秩
+	Rank insert(Rank r, T const& t);
+
+	//区间删除,返回删除元素个数
+	int remove(Rank lo, Rank hi);
+
+	//单元素删除,返回被删除的元素
+	T remove(Rank r);
 };
 
 
+
+//内部函数
+//递增扩容
+template<class T>
+void MyVector<T>::expandAdd()
+{
+	if (_size < _capacity)
+		return;
+	if (_capacity < DEFAULT_CAPACITY)//不低于最小容量
+		_capacity = DEFAULT_CAPACITY;
+
+	T *old_elem = _elem;
+	_elem = new T[_capacity += INCREMENT];//新开辟
+	for (int i = 0; i < _size; i++)
+	{
+		_elem[i] = old_elem[i];
+	}
+	delete[] old_elem;//释放旧堆区空间
+}
+
+//加倍扩容
+template<class T>
+void MyVector<T>::expandDouble()
+{
+	if (_size < _capacity)
+		return;
+	if (_capacity < DEFAULT_CAPACITY)//不低于最小容量
+		_capacity = DEFAULT_CAPACITY;
+
+	T *old_elem = _elem;
+	_elem = new T[_capacity <<= 1];
+	for (int i = 0; i < _size; i++)
+	{
+		_elem[i] = old_elem[i];
+	}
+	delete[] old_elem;
+}
+
+//缩容
+template<class T>
+void MyVector<T>::shrink()
+{
+	if (_capacity < DEFAULT_CAPACITY << 1)//不致于收缩到DEFAULT_CAPACITY以下
+		return;
+	if (_size > _capacity >> 2)//25%装填因子为界
+		return;
+	T *old_elem = _elem;
+	_elem =	new T[_capacity >>= 1];//容量减半, 更新容量
+	for (int i = 0; i < _size; i++)//复制原容器元素
+	{
+		_elem[i] = old_elem[i];
+	}
+	delete[] old_elem;//释放旧容器
+	old_elem = NULL;
+}
 
 //构造函数
 //默认 构造函数
@@ -69,13 +144,14 @@ template<class T>
 MyVector<T>::MyVector(int c = DEFAULT_CAPACITY)
 {
 	_size = 0;
-	_elem = new T[_capacity = c];
+	_elem = new T[_capacity = max(c, DEFAULT_CAPACITY)];
 }
 
 //拷贝数组区间 构造函数
 template<class T>
 MyVector<T>::MyVector(T const* A, Rank lo, Rank hi)
 {
+	assert(A != NULL);
 	copyFrom(A, lo, hi);
 }
 
@@ -83,6 +159,7 @@ MyVector<T>::MyVector(T const* A, Rank lo, Rank hi)
 template<class T>
 MyVector<T>::MyVector(MyVector<T> const& V, Rank lo, Rank hi)
 {
+	assert(V._elem != NULL);
 	copyFrom(V._elem, lo, hi);
 }
 
@@ -107,12 +184,11 @@ void MyVector<T>::copyFrom(T const* A, Rank lo, Rank hi)
 
 
 
-
 //析构函数
 template<class T>
 MyVector<T>::~MyVector()
 {
-	assert(_elem);
+	assert(_elem != NULL);
 	delete[] _elem;
 	_elem = NULL;
 	_size = 0;
@@ -123,6 +199,22 @@ MyVector<T>::~MyVector()
 
 
 //只读接口
+//读取 秩 元素
+template<class T>
+T& MyVector<T>::get(Rank i)
+{
+	assert(i >= 0 && i <= _size);
+	return _elem[i];
+}
+
+//重载[]运算符
+template<class T>
+T& MyVector<T>::operator[](Rank r)
+{
+	assert(r >= 0 && r <= _size);
+	return _elem[r];
+}
+
 //返回向量大小
 template<class T>
 Rank MyVector<T>::size()
@@ -137,46 +229,60 @@ int MyVector<T>::capacity()
 	return _capacity;
 }
 
-//重载[]运算符
-template<class T>
-T MyVector<T>::operator[](Rank i)
-{
-	return _elem[i];
-}
 
 
 
 //只写接口
-//递增扩容
+//将 秩 元素 换成 指定元素
 template<class T>
-void MyVector<T>::expendAdd()
+void MyVector<T>::put(Rank r, T t)
 {
-	if (_size < _capacity)
-		return;
-	_capacity = max(_capacity, DEFAULT_CAPACITY);//不低于最小容量
-
-	T *old_elem = _elem;
-	_elem = new T[_capacity += INCREMENT];//新开辟
-	for (int i = 0; i < _size; i++)
-	{
-		_elem[i] = old_elem[i];
-	}
-	delete[] old_elem;//释放旧堆区空间
+	assert(r >= 0 && r <= _size);
+	_elem[r] = t;
 }
 
-//加倍扩容
+//在秩r处插入元素t,返回秩
 template<class T>
-void MyVector<T>::expendDouble()
+Rank MyVector<T>::insert(Rank r, T const& t)
 {
-	if (_size < _capacity)
-		return;
-	_capacity = max(_capacity, DEFAULT_CAPACITY);//不低于最小容量
+	assert(r >= 0 && r <= _size);
 
-	T *old_elem = _elem;
-	_elem = new T[_capacity <<= 1];
-	for (int i = 0; i < _size; i++)
+	expandDouble();
+	
+	for (int i = _size; i > r; i--)//后缀后挪一位
 	{
-		_elem[i] = old_elem[i];
+		_elem[i] = _elem[i - 1];
 	}
-	delete[] old_elem;
+	_elem[r] = t;//放入元素
+	_size++;//更新大小
+	return r;
+}
+
+//区间删除,返回删除元素个数
+template<class T>
+int MyVector<T>::remove(Rank lo, Rank hi)
+{
+	assert(0 <= lo&&lo <= hi&&hi <= _size);
+
+	if (lo == hi)
+		return 0;
+
+	while (hi < _size)
+	{
+		_elem[lo++] = _elem[hi++];
+	}
+	_size = lo;//更新大小
+	shrink();//若装填因子过低，缩容
+	return hi - lo;
+}
+
+//单元素删除,返回被删除的元素
+template<class T>
+T MyVector<T>::remove(Rank r)
+{
+	assert(r >= 0 && r <= _size);
+
+	T tmp = _elem[r];//备份被删除元素
+	remove(r, r + 1);
+	return tmp;//返回被删除元素
 }
--- 21_2/21_2_11/源.cpp
+++ 21_2/21_2_11/源.cpp
@@ -4,27 +4,40 @@ using namespace std;
 
 void Test01()
 {
-	int arr[] = { 1, 2, 3, 5, 8, 4, 8 };
+	int arr[] = { 0, 1, 2, 3, 4, 3, 2, 1, 0 };
 	//MyVector<int> mv;//默认 构造
 	MyVector<int> mv(arr, 0, sizeof(arr)/sizeof(arr[0]));//拷贝 数组区间 构造
-	MyVector<int> mv1(mv, 0, 3);//拷贝 向量区间 构造
+	MyVector<int> mv1(mv, 0, mv.size());//拷贝 向量区间 构造
 	MyVector<int> mv2(mv);//拷贝 向量整体 构造
-	for (int i = 0; i < mv.size(); i++)
+	cout << "mv :  ";
+	for (int i = 0; i < 10; i++)//往 mv中插入十个元素
+	{
+		mv.insert(i, 10 * i);
+	}
+	mv.remove(0, 10);//删除前十个元素
+	mv.remove(mv.size()-1);//删除最后一个元素
+	for (int i = 0; i < mv.size(); i++)//重载操作符[]的方式 遍历mv
 	{
 		cout << mv[i] << " ";
 	}
 	cout << endl;
 
-	for (int i = 0; i < mv1.size(); i++)
+
+	cout << "mv1:  ";
+	for (int i = 0; i < mv1.size(); i++)//get的方式 遍历mv1
 	{
-		cout << mv1[i] << " ";
+		cout << mv1.get(i) << " ";
 	}
 	cout << endl;
 
+
+	cout << "mv2:  ";
 	for (int i = 0; i < mv2.size(); i++)
 	{
+		mv2.put(i, mv2.get(i)*2);//替换mv2中的元素,加倍
 		cout << mv2[i] << " ";
 	}
+	cout << endl;
 }
 
 
