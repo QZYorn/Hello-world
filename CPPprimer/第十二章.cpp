667690c9442e3fc6ab88e37a6b0d0ac62b077887
QZYorn <864239480@qq.com>
2021-03-31 22:54:55 +0800

    CPPprimer—test—12.33

--- CPPprimer/第十二章.cpp
+++ CPPprimer/第十二章.cpp
@@ -7,6 +7,7 @@
 #include<vector>
 #include<list>
 #include<map>
+#include<set>
 #include<deque>
 #include<forward_list>
 #include<algorithm>
@@ -235,6 +236,8 @@ void test12_1_5()
 
 void test12_1_6()
 {
+	cout << "\n12.1.6" << endl;
+
 	ifstream ifs("test.txt");
 	if (!ifs.is_open())
 	{
@@ -255,10 +258,181 @@ void test12_1_6()
 	ifs.close();
 }
 
+void test12_2_1()
+{
+	cout << "\n12.2.1" << endl;
+
+	//12.23
+	char* chr_ptr = new char[10];
+	strcpy(chr_ptr, "aaa");
+	strcat(chr_ptr + strlen("aaa"), "ccc");
+	/*for (auto it = chr_ptr; it != chr_ptr + 10; ++it)
+	{
+		cout << *it;
+	}*/
+	cout << chr_ptr << endl;
+	delete[] chr_ptr;
+	chr_ptr = nullptr;
+
+	string s1{ "aaa2" };
+	string s2{ "ccc2" };
+	char* chr_ptr2 = new char[s1.size()+s2.size()];
+	strcpy(chr_ptr2, (s1+s2).c_str());
+	/*for (auto it = chr_ptr2; it != chr_ptr2 + s1.size() + s2.size(); ++it)
+	{
+		cout << *it;
+	}*/
+	cout << chr_ptr2 << endl;
+
+	//12.24
+	unique_ptr<char[]> up(new char[5]());
+	//unique_ptr<char[]> nup;
+	string s;
+	while (cin >> s)
+	{
+		int old_size = sizeof(up.get());
+		if (s.size() > old_size)
+		{
+			int new_size = s.size()*1.5;
+			unique_ptr<char[]> nup(new char[new_size]);
+			swap(nup,up);
+		}
+		strcpy(up.get(), s.c_str());
+	}
+	cout << up.get() << endl;
+
+	//12.25
+	int *pa = new int[10];
+	delete[] pa;
+
+}
+
+void test12_2_2()
+{
+	cout << "\n12.2.2" << endl;
+
+	//12.26
+	allocator<string> al;
+	auto const p = al.allocate(10);//首元素
+	auto q = p;//尾元素
+	string s;
+	while (cin >> s && q != p + 10)
+	{
+		al.construct(q++, s);
+	}
+	const size_t size = q - p;
+	while (q != p)
+	{
+		al.destroy(--q);
+	}
+	al.deallocate(p,10);
+}
+
+
+class QueryResult;
+class TextQuery
+{
+public:
+	typedef size_t line_no;
+	TextQuery(ifstream &ifs) :file(new vector<string>)
+	{
+		string s;
+		int curr = 0;
+		while (getline(ifs,s))
+		{
+			file->push_back(s);
+			string word;
+			istringstream iss(s);
+			while (iss >> word)
+			{
+				auto &line = word_line[word];
+				if (!line)
+				{
+					line.reset(make_shared<set<line_no>>);
+				}
+				line->insert(curr);
+			}
+			++curr;
+		}
+	}
+
+	QueryResult query(const string& s) const;
+
+private:
+
+	shared_ptr<vector<string>> file;
+	map<string, shared_ptr<set<line_no>>> word_line;
+};
+
+class QueryResult
+{
+	friend ostream& print(ostream& os, QueryResult& qr);
+
+	typedef size_t line_no;
+public:
+	QueryResult(string s,
+		shared_ptr<vector<string>> p,
+		shared_ptr<set<line_no>> l)
+		:word(s), file(p), line(l){}
+private:
+	string word;
+	shared_ptr<vector<string>> file;
+	shared_ptr<set<line_no>> line;
+
+};
+
+QueryResult TextQuery::query(const string& s) const
+{
+	auto line = word_line.find(s);
+	static shared_ptr<set<line_no>> nodata(new set<line_no>);
+	if (line == word_line.end())
+	{
+		return{ s, file, nodata};
+	}
+	return{ s, file, line->second };
+}
+
+ostream& print(ostream& os, QueryResult& qr)
+{
+	os << qr.word << " occurs "
+		<< qr.line->size() << (1 < qr.line->size() ? " times " : " time ")
+		<< endl;
+	for (auto c : *qr.line)
+	{
+		os << "\t(line" << c+1  << ")"
+			<< (*qr.file)[c] << endl;
+	}
+	return os;
+}
+
+void runQuries(ifstream &infile)
+{
+	TextQuery tq(infile);
+	while (true)
+	{
+		cout << "输入要查找的单词，或输入q退出: ";
+		string s;
+		if (!(cin >> s) || s == "q")
+			break;
+		print(cout, tq.query(s)) << endl;
+	}
+}
+
+void test12_3_1()
+{
+	cout << "\n12.3.1" << endl;
+
+	//12.27
+	
+}
+
 int main()
 {
 	//test12_1_2();
 	//test12_1_5();
-	test12_1_6();
+	//test12_1_6();
+	//test12_2_1();
+	//test12_2_2();
+	test12_3_1();
 	return 0;
 }
