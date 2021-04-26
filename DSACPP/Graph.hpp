6d0d3ebe944262c837083e107f2d1ac0f491378c
QZYorn <864239480@qq.com>
2021-04-26 20:10:17 +0800

    DSACPP—Graph—深度优先遍历

--- DSACPP/Graph.hpp
+++ DSACPP/Graph.hpp
@@ -73,17 +73,45 @@ void Graph<Tv, Te>::BFS(int v, int &clock)//(连通域)广度优先搜索算法
 		int v = Q.dequeue();//队首出队
 		dTime(v) = ++clock;//更新时间标签dTime
 		for (int u = firstNbr(v); -1 < u; u = nextNbr(v, u))//初始化需访问顶点u为首邻接顶点，直到u越界，每次迭代u转为下一邻接顶点
-			/*
-
-			*/
-			;
+		{
+			if (status(u) == UNDISCOVERED)//若邻接顶点处于“未发现”的状态即未入队
+			{
+				Q.enqueue(u);//则将u邻接顶点入队
+				status(u) = DISCOVERED;//并将u置为“已发现”
+				status(v, u) = TREE;//关联边(v,u)引入作为“树边”
+				parent(u) = v;//u在树中的父节点为v
+			}
+			else//若邻接顶点为“已发现”即已入队 或 为“已访问”即已出队
+				status(v, u) = CROSS;//将(v,u)归类为“跨边”
+		}//v的所有邻接顶点访问完毕
 		status(v) = VISITED;//至此，当前顶点访问完毕，v状态置为“已访问”
-	}
+	}//最终形成一支遍历支撑树
 }
 
 template<typename Tv, typename Te>
-void Graph<Tv, Te>::DFS(int, int&)//(连通域)深度优先搜索算法
+void Graph<Tv, Te>::DFS(int v, int &clock)//(连通域)深度优先搜索算法
 {
+	status(v) = DISCOVERED;//v顶点状态置为“已发现”
+	dTime(v) = ++clock;
+	for (int u = firstNbr(v); -1 < u; u = nextNbr(v, u))//枚举v的所有邻接顶点
+	{
+		switch (status(u))
+		{
+		case UNDISCOVERED://“未发现”
+			status(v, u) = TREE; //(v,u)边引入作为“树边”
+			parent(u) = v;		 //支撑树中u父亲指向v
+			DFS(u, clock);		 //以u为首顶点递归调用DFS
+			break;
+		case DISCOVERED:  //“已发现”，但尚未访问完毕，则此顶点应属被后代指向的祖先
+			status(v, u) = BACKWARD;//(v,u)边状态置为“后向边”，即“回边”，后代指向祖先
+			break;
+		default:		  //“已访问”，则依据v,u访问的次序，将(v,u)状态置为“前向边”或“跨边”
+			status(v, u) = dTime(v) < dTime(u) ? FORWARD : CROSS;//“前向边”祖先指向后代，“跨边”无直属关系的不同分支
+			break;
+		}
+	}
+	status(v) = VISITED;//v顶点状态置为“已访问”
+	fTime(v) = ++clock;
 
 }
 
@@ -107,15 +135,27 @@ template<typename PU> void Graph<Tv, Te>::PFS(int, PU)//(连通域)优先级搜索框架
 
 //算法
 template<typename Tv,typename Te>
-void Graph<Tv, Te>::bfs(int)//广度优先搜索算法
+void Graph<Tv, Te>::bfs(int s)//广度优先搜索算法
 {
-	
+	reset(); int clock = 0; int v = s;//初始化
+	do//逐一检查各顶点
+	{
+		if (UNDISCOVERED == status(v))//若顶点状态为“未发现”
+			BFS(v, clock);//则从该顶点出发进行一次广度优先遍历
+	} while (s != v = ( ++v % n));//按序号检查，故不漏不重
+	//从s顶点出发，向后依次遍历至n，再回头到0开始遍历,再到s-1。故无论起始于何处都能遍历完全
 }
 
 template<typename Tv, typename Te>
 void Graph<Tv, Te>::dfs(int)//深度优先搜索算法
 {
-
+	reset(); int clock = 0; int v = s;//初始化
+	do//逐一检查各顶点
+	{
+		if (UNDISCOVERED == status(v))//若顶点状态为“未发现”
+			DFS(v, clock);//则从该顶点出发进行一次广度优先遍历
+	} while (s != v = (++v % n));//按序号检查，故不漏不重
+	//从s顶点出发，向后依次遍历至n，再回头到0开始遍历,再到s-1。故无论起始于何处都能遍历完全
 }
 
 template<typename Tv, typename Te>
